5c4b886bd96675ce7383fa256b9752a3c29ea7e6
Rodrigo Agerri <agerri.rodrigo@gmail.com>
2014-04-08 15:02:14 +0200

    small code cleaning

--- src/main/java/es/ehu/si/ixa/pipe/nerc/eval/package-info.java
+++ src/main/java/es/ehu/si/ixa/pipe/nerc/eval/package-info.java
@@ -2,7 +2,7 @@
  * Package containing common code for evaluation of ixa-pipe-nerc.
  * <ol>
  * <li>Using Probabilistic Models
- * <li>Using reference and test corpora 
+ * <li>Using reference and test corpora
  * </ol>
  */
 package es.ehu.si.ixa.pipe.nerc.eval;
