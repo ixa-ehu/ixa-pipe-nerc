1f2fc6545820f4c2a950ba446e5e71f985b946b9
Rodrigo Agerri <agerri.rodrigo@gmail.com>
2014-04-26 12:39:28 +0200

    minor bug in class loader

--- src/main/java/es/ehu/si/ixa/pipe/nerc/StatisticalNameFinder.java
+++ src/main/java/es/ehu/si/ixa/pipe/nerc/StatisticalNameFinder.java
@@ -245,7 +245,7 @@ public class StatisticalNameFinder implements NameFinder {
     InputStream trainedModelInputStream = null;
     if (lang.equalsIgnoreCase("en")) {
       trainedModelInputStream = getClass().getResourceAsStream(
-          "/en/en-nerc-perceptron-conll03-ontonotes-4.0-4-types-baseline-c0-b3-testa.bin");
+          "/en/en-nerc-perceptron-conll03-ontonotes-4.0-4-types-baseline-c0-b3.bin");
     }
     if (lang.equalsIgnoreCase("es")) {
       trainedModelInputStream = getClass().getResourceAsStream(
