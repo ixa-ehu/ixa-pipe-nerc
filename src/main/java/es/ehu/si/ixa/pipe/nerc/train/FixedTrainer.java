b9f8398edb01927d75ccaafebb700c45f3a57b51
Rodrigo Agerri <ragerri@apache.org>
2014-09-26 17:50:09 +0200

    best results so far with brown 1000 clark 600 previous map 2 + windows, faster models

--- src/main/java/es/ehu/si/ixa/pipe/nerc/train/FixedTrainer.java
+++ src/main/java/es/ehu/si/ixa/pipe/nerc/train/FixedTrainer.java
@@ -242,7 +242,6 @@ public class FixedTrainer extends AbstractTrainer {
         dictionaries = new Dictionaries(dictPath);
       }
       addDictionaryFeatures(leftWindow, rightWindow, featureList);
-      //addDictionaryFeatures(featureList);
     }
     
     String brownParam = InputOutputUtils.getBrownFeatures(params);
@@ -250,7 +249,6 @@ public class FixedTrainer extends AbstractTrainer {
       System.err.println("-> Brown clusters features added!");
       brownLexicon = setBrownResources(params, brownParam);
       addBrownFeatures(leftWindow, rightWindow, featureList);
-      //addBrownFeatures(featureList);
     }
     
     String clarkParam = InputOutputUtils.getClarkFeatures(params);
@@ -261,7 +259,6 @@ public class FixedTrainer extends AbstractTrainer {
         clarkCluster = new ClarkCluster(clarkPath);
       }
       addClarkFeatures(leftWindow, rightWindow, featureList);
-      //addClarkFeatures(featureList);
     }
     String word2vecClusterParam = InputOutputUtils.getWord2VecClusterFeatures(params);
     if (word2vecClusterParam.equalsIgnoreCase("yes")) {
@@ -315,10 +312,7 @@ public class FixedTrainer extends AbstractTrainer {
    */
   public static void addPreviousMapFeatures(int leftWindow, int rightWindow,
       List<AdaptiveFeatureGenerator> featureList) {
-    //TODO clarify this and other Ratinov and Roth baseline features
     featureList.add(new PreviousMapFeatureGenerator());
-    //featureList.add(new Prev2MapFeatureGenerator());
-    //featureList.add(new WindowFeatureGenerator(leftWindow, rightWindow, new PreviousMapTokenFeatureGenerator()));
   }
   
   /**
@@ -406,24 +400,10 @@ public class FixedTrainer extends AbstractTrainer {
     for (int i = 0; i < dictionaries.getIgnoreCaseDictionaries().size(); i++) {
       prefix = dictionaries.getDictNames().get(i);
       dictionary = dictionaries.getIgnoreCaseDictionaries().get(i);
-      featureList.add(new WindowFeatureGenerator(leftWindow, rightWindow, new DictionaryFeatureGenerator(prefix, dictionary)));
+      featureList.add(new WindowFeatureGenerator(new DictionaryFeatureGenerator(prefix, dictionary), leftWindow, rightWindow));
     }
   }
   
-  /**
-   * Adds the dictionary features of the curren token in a given window.
-   * @param leftWindow the left window
-   * @param rightWindow the right window
-   * @param featureList the feature list
-   */
-  private static void addDictionaryFeatures(
-      final List<AdaptiveFeatureGenerator> featureList) {
-    for (int i = 0; i < dictionaries.getIgnoreCaseDictionaries().size(); i++) {
-      prefix = dictionaries.getDictNames().get(i);
-      dictionary = dictionaries.getIgnoreCaseDictionaries().get(i);
-      featureList.add(new DictionaryFeatureGenerator(prefix, dictionary));
-    }
-  }
   /**
    * Adds Brown classes features for each token feature in a given window.
    * @param leftWindow the leftwindow value
@@ -434,20 +414,10 @@ public class FixedTrainer extends AbstractTrainer {
     featureList.add(new WindowFeatureGenerator(new BrownTokenFeatureGenerator(brownLexicon), leftWindow, rightWindow));
     featureList.add(new WindowFeatureGenerator(new BrownTokenClassFeatureGenerator(brownLexicon), leftWindow, rightWindow));
     featureList.add(new BrownBigramFeatureGenerator(brownLexicon));
+    featureList.add(new Prev2MapFeatureGenerator());
+    featureList.add(new WindowFeatureGenerator(new PreviousMapTokenFeatureGenerator(), leftWindow, rightWindow));
   }
 
-  /**
-   * Adds Brown classes features for each token feature in a given window.
-   * @param leftWindow the leftwindow value
-   * @param rightWindow the rightwindow value
-   * @param featureList the feature list to which the feature generator is added
-   */
-  private static void addBrownFeatures(final List<AdaptiveFeatureGenerator> featureList) {
-    featureList.add(new BrownTokenFeatureGenerator(brownLexicon));
-    featureList.add(new BrownTokenClassFeatureGenerator(brownLexicon));
-    featureList.add(new BrownBigramFeatureGenerator(brownLexicon));
-  }
-  
   /**
    * Add the Clark cluster class of the current token as a feature in a given window.
    * @param leftWindow the left window
@@ -459,16 +429,6 @@ public class FixedTrainer extends AbstractTrainer {
     featureList.add(new WindowFeatureGenerator(new ClarkFeatureGenerator(clarkLexicon), leftWindow, rightWindow));
   }
   
-  /**
-   * Add the Clark cluster class of the current token as a feature in a given window.
-   * @param leftWindow the left window
-   * @param rightWindow the right window
-   * @param featureList the feature list
-   */
-  private static void addClarkFeatures(final List<AdaptiveFeatureGenerator> featureList) {
-    clarkLexicon = clarkCluster.getIgnoreCaseDictionary();
-    featureList.add(new ClarkFeatureGenerator(clarkLexicon));
-  }
   /**
    * Add the word2vec cluster class feature of the curren token in a given window.
    * @param leftWindow the left window
