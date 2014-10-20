55012ffc6f42aac84769d57320a552b8ec65a092
Rodrigo Agerri <ragerri@apache.org>
2014-10-20 15:53:12 +0200

    code cleaning

--- src/main/java/es/ehu/si/ixa/pipe/nerc/features/DictionaryFeatureGenerator.java
+++ src/main/java/es/ehu/si/ixa/pipe/nerc/features/DictionaryFeatureGenerator.java
@@ -16,7 +16,6 @@
 
 package es.ehu.si.ixa.pipe.nerc.features;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
@@ -24,7 +23,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import opennlp.tools.cmdline.CmdLineUtil;
 import opennlp.tools.util.InvalidFormatException;
 import opennlp.tools.util.Span;
 import opennlp.tools.util.featuregen.ArtifactToSerializerMapper;
