675193039a8ec46ef45b4e93b12fc1b03e506d57
Rodrigo Agerri <ragerri@apache.org>
2015-11-13 11:57:13 +0100

    update opennlp to 1.6.0 and cluster null feature

--- src/main/java/eus/ixa/ixa/pipe/nerc/features/CharacterNgramFeatureGenerator.java
+++ src/main/java/eus/ixa/ixa/pipe/nerc/features/CharacterNgramFeatureGenerator.java
@@ -43,7 +43,6 @@ public class CharacterNgramFeatureGenerator extends CustomFeatureGenerator {
 
     NGramModel model = new NGramModel();
     model.add(tokens[index], Integer.parseInt(attributes.get("minLength")), Integer.parseInt(attributes.get("maxLength")));
-    //model.add(tokens[index], Integer.parseInt(attributes.get("minLength")), tokens[index].length());
     for (StringList tokenList : model) {
 
       if (tokenList.size() > 0) {
